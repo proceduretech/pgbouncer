7d1a62873e135e42c2986295d333a0e425043a81
Sreeraj Rajan <chelseabluerockers@gmail.com>
2020-07-02 12:30:40 +0530

    Increase password length to 2048 (#1)

--- include/bouncer.h
+++ include/bouncer.h
@@ -134,7 +134,7 @@ extern int cf_sbuf_len;
 #define MAX_DBNAME	64
 #define MAX_USERNAME	64
 /* typical SCRAM-SHA-256 verifier takes at least 133 bytes */
-#define MAX_PASSWORD	160
+#define MAX_PASSWORD	2048
 
 /*
  * AUTH_* symbols are used for both protocol handling and
